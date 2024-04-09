f89c063895602ad5946f0cc32f7b2e526f267361
Anthony da Costa <anthonydacsilva10@gmail.com>
2024-04-09 14:34:09 -0300

    feat: adiciona o requiremento dos dados para utilização no modal de informações de cada pokemom

--- js/poke-API.js
+++ js/poke-API.js
@@ -1,6 +1,7 @@
 const pokeAPI = { }
 
-function convertPokeAPIDetailToPokemon(pokeDetail){
+async function convertPokeAPIDetailToPokemon(pokeDetail){
+  //POKEMON-BOX DETAIL
   const pokemon = new Pokemon();
   pokemon.number = pokeDetail.order
   pokemon.name = pokeDetail.name
@@ -12,23 +13,38 @@ function convertPokeAPIDetailToPokemon(pokeDetail){
   pokemon.type = type
 
   pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+  
+  //POKEMON-INFO DETAILS
+  pokemon.height = `${(pokeDetail.height / 10).toFixed(1)}m`
+  pokemon.weight = `${(pokeDetail.weight / 10).toFixed(1)}kg`
+
+  const descriptionReq = await fetch(pokeDetail.species.url);
+  const descriptionData = await descriptionReq.json();
+  const descriptions = descriptionData.flavor_text_entries;
+  
+  const englishDescription = descriptions.find(entry => entry.language.name === 'en');
+  pokemon.description = englishDescription.flavor_text;
 
   return pokemon
 }
 
-pokeAPI.getPokemonDetail = (pokemon) => {
-  return fetch(pokemon.url)
-  .then((response) => response.json())
-  .then(convertPokeAPIDetailToPokemon)
+pokeAPI.getPokemonDetail = async (pokemon) => {
+  const req = await fetch(pokemon.url)
+  const resp = await req.json()
+  return convertPokeAPIDetailToPokemon(resp)
 }
 
-pokeAPI.getPokemon = (offset = 0, limit = 151) => {
-  const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}offset=${offset}`
-  return fetch(url)
-  .then(resp => resp.json())
-  .then(data => data.results)
-  .then((pokemon) => pokemon.map(pokeAPI.getPokemonDetail))
-  .then((detailRequests) => Promise.all(detailRequests))
-  .catch(e => console.log(e))
+pokeAPI.getPokemon = async (offset, limit) => {
+  try{
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    const dataReq = await fetch(url)
+    const dataResp = await dataReq.json()
+    const dataResults = await dataResp.results
+    const detailRequests = dataResults.map(pokeAPI.getPokemonDetail)
+    return Promise.all(detailRequests)
+  }
+  catch{
+    console.log('Error!')
+  }
 }
 
