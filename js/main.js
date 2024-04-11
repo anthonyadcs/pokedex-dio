bcf6ba554bce3abb3e91ca42adbc489a966430a3
Anthony da Costa <anthonydacsilva10@gmail.com>
2024-04-11 14:44:22 -0300

    feat: adiciona os atributos(peso, altura e descrição) dos Pokemon no modal de informações

--- js/main.js
+++ js/main.js
@@ -3,11 +3,14 @@ const loadMoreButton = document.querySelector('#load-more')
 const pokeInfoBox = document.querySelector('.pokemon-info')
 const limit = 5;
 let offset = 0;
+let pokemonAttributes = [];
 
 //Função que constrói todas as pokemon-boxs
 function loadPokemonItens(offset, limit){
   pokeAPI.getPokemon(offset, limit).then((pokemonList = []) => {
     const pokeHTML = pokemonList.map((pokemon) => {
+      pokemonAttributes.push({number: pokemon.number, description: pokemon.description, height: pokemon.height, weight: pokemon.weight});
+
       return `
       <li class="pokemon-box ${pokemon.type}">
       <span class="number"><p>${pokemon.number}</p></span>
@@ -27,6 +30,7 @@ function loadPokemonItens(offset, limit){
     }).join('');
     olPokemon.innerHTML += pokeHTML;
 
+
     //Loop que adiciona um escutador de evento em cada pokemon-box para criação do modal
     const pokeBoxs = document.querySelectorAll('.pokemon-box')
     pokeBoxs.forEach((pokeBox) => {
@@ -37,6 +41,7 @@ function loadPokemonItens(offset, limit){
   })
   
 }
+
 //Função que captura o elemento-pai do elemento enviado.
 function findAncestorWithClass(element, className){
   let currentElement = element
@@ -55,45 +60,72 @@ function findAncestorWithClass(element, className){
 loadMoreButton.addEventListener('click', () => {
   offset += limit
   loadPokemonItens(offset, limit)
+  console.log(pokemonAttributes);
 })
 
 //Função que cria o modal de cada um dos Pokemon ao clicar em sua box.
 const showPokeInfo = (e) => {
+  
   const target = e.target;
   const pokeBox = findAncestorWithClass(target, 'pokemon-box')
   const pokemonInfos = pokeBox.children
+  let pokemonDescription
+  let pokemonHeight
+  let pokemonWeight
+
+  pokemonAttributes.forEach((pokemonAttributes) => {
+    if(pokemonAttributes.number == Number(pokemonInfos[0].outerText)){
+      pokemonDescription = pokemonAttributes.description
+      pokemonHeight = pokemonAttributes.height
+      pokemonWeight = pokemonAttributes.weight
+    }
+  })
   
   const pokeInfoHTML = `
   <h1>${pokemonInfos[1].innerText}</h1>
   <ol class="types">
-  <li class="type">${pokemonInfos[2].children[0].children[0].innerText}</li>
-  ${pokemonInfos[2].children[0].children[1] ? `<li>${pokemonInfos[2].children[0].children[1].innerText}</li>` : ''}
+    <li class="type">
+    <p class="${pokemonInfos[2].children[0].children[0].outerText}">
+    ${pokemonInfos[2].children[0].children[0].innerText}
+    </p>
+    </li>
+    <p>
+    ${pokemonInfos[2].children[0].children[1] ? `<li class="type">
+    <p class="${pokemonInfos[2].children[0].children[1].outerText}">
+    ${pokemonInfos[2].children[0].children[1].innerText}
+    </p>
+    </li>` : ''}
+    </p>
   </ol>
   <img
   src="${pokemonInfos[2].children[1].currentSrc}"
   alt=""
   />
   
-  <div class="info-container fire">
-  <h3>About</h3>
-  <div class="infos" >
-  <span class="description"
-  ><p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus,
-  architecto.
-  </p></span>
+  <div class="info-container">
+    <h3>About</h3>
+    <div class="infos">
+      <span class="description">
+        <p>
+          ${pokemonDescription}
+        </p>
+      </span>
   
-  <ol class="poke-info-ol">
-  <li>Height:</li>
-  <li>Weight:</li>
-    <li>Gender:</li>
-    <li>Category:</li>
+    <ol class="poke-info-ol">
+      <li>
+      <p>Height</p>
+      <p>${pokemonHeight}</p>
+      </li>
+      <li>
+      <p>Weight</p>
+      <p>${pokemonWeight}</p>
+      </li>
     </ol>
-    </div>
-    </div>
+  </div>
+</div>
     `
     pokeInfoBox.innerHTML = pokeInfoHTML
-    pokeInfoBox.style.display = 'block'
+    pokeInfoBox.style.display = 'flex'
   }
 
 //Primeira chamada da função que carrega a lista de Pokemon
